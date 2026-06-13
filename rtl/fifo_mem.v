3ce47d8adc96f4cfadde1b7582dc25218ccd17eb
Shubham Shreekumar <shubham.shreekumar@gmail.com>
2026-06-13 16:59:03 +0530

    Clean up comments in fifo_mem.v
    Removed header comments from fifo_mem.v

--- rtl/fifo_mem.v
+++ rtl/fifo_mem.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02.06.2026 22:50:44
-// Design Name: 
-// Module Name: fifo_mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module fifo_mem #(
     parameter DATA_WIDTH = 8,
